cd97631cb08dbfabb2ab8e6c158280c18d37334f
Muhammad Rizky Yusfian Yusuf <rizky.yusfian16@gmail.com>
2023-08-02 12:40:15 +0900

    create body components

---
+++ src/components/Body.js
@@ -0,0 +1,12 @@
+import '../styles/body.css';
+// import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+// import { icon } from '@fortawesome/fontawesome-svg-core/import.macro'
+
+// spotify sidebar
+const Body = () => {
+    return (
+        <div className="body">this is body</div>
+    )
+}
+
+export default Body;
