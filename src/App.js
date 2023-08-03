2ad194732d79cc6210fb526fff49430cf5d51424
Muhammad Rizky Yusfian Yusuf <rizky.yusfian16@gmail.com>
2023-08-04 01:36:09 +0900

    Update layout component for reusable purpose

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
 // import logo from './logo.svg';
 import './App.css';
 import Layout from './components/layout/Layout.js';
+// import HomePage from './components/homePage/HomePage.js';
+// import Body from './components/body/Body';
 
 function App() {
+
   return (
     <div className="App">
-      <Layout />
+      <Layout/>
     </div>
   );
 }
