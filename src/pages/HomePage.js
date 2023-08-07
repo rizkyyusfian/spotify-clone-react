f8f757f4afbec9515eabe4df2bb99f942ff23953
Muhammad Rizky Yusfian Yusuf <rizky.yusfian16@gmail.com>
2023-08-08 01:38:08 +0900

    Update HomePage.js

--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -1,18 +1,24 @@
+import { useContext } from 'react';
 import '../styles/homepage.css';
 import MusicSection from '../components/music/MusicSection';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { regular } from '@fortawesome/fontawesome-svg-core/import.macro'
+import LoginContext from '../contexts/LoginContext';
 
 const HomePage = () => {
+    const { isLoggedIn } = useContext(LoginContext);
+
     return (
         <div className='body'>
-            <div className='section'>
-                <div className="section--header">
-                    <div className="section--header--left">
-                        <h2 className="title">Welcome back <FontAwesomeIcon icon={regular("hand")} /></h2>
+            {isLoggedIn &&
+                <div className='section'>
+                    <div className="section--header">
+                        <div className="section--header--left">
+                            <h2 className="title">Welcome back <FontAwesomeIcon icon={regular("hand")} /></h2>
+                        </div>
                     </div>
                 </div>
-            </div>
+            }
             <MusicSection
                 header="Recently Played" />
             <MusicSection
